85678d09c7b258f7b6dc1dac1b96a0e521108885
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2023-12-01 04:29:51 +0900

    현재 위치로 초기화

--- app/src/main/java/com/example/forforeiner/Activity/nearLawCenter.java
+++ app/src/main/java/com/example/forforeiner/Activity/nearLawCenter.java
@@ -4,57 +4,51 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.AsyncTask;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
+import android.os.Looper;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.forforeiner.BuildConfig;
 import com.example.forforeiner.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.kakao.vectormap.KakaoMap;
 import com.kakao.vectormap.KakaoMapReadyCallback;
+import com.kakao.vectormap.LatLng;
 import com.kakao.vectormap.MapLifeCycleCallback;
 import com.kakao.vectormap.MapView;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
-import Dto.LocalQADto.ResponseDto.AnswerInfo;
-import Dto.LocalQADto.ResponseDto.Example;
-import Dto.MapGeocoderDto.ResponseDto.GeocoderDto;
-import Retrofit.MapRetrofitClient;
-import Retrofit.MapRetrofitInterface;
-import Retrofit.RetrofitClient;
-import Retrofit.RetrofitInterface;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import dalvik.annotation.optimization.CriticalNative;
 
 public class nearLawCenter extends AppCompatActivity {
 
     private EditText et;
     final int REQ_PERMISSION_CODE = 100;
+
+    public static final String GPS_PROVIDER = "gps";
+    public static final String NETWORK_PROVIDER = "network";
+    public static final String PASSIVE_PROVIDER = "passive";
+
+    FusedLocationProviderClient flpClient;
+    Location last_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +56,81 @@ public class nearLawCenter extends AppCompatActivity {
         setContentView(R.layout.activity_near_law_center);
 
         et = findViewById(R.id.location);
+
+        checkPermission();
+        flpClient = LocationServices.getFusedLocationProviderClient(this);
+        flpClient.requestLocationUpdates(
+                getLocationRequest(),
+                mLocCallback,
+                Looper.getMainLooper()
+        );
+    }
+
+    LocationCallback mLocCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            for (Location loc : locationResult.getLocations()) {
+                double lat = loc.getLatitude();
+                double lng = loc.getLongitude();
+
+                getKakaoMap(lat, lng);
+            }
+        }
+    };
+
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setNumUpdates(1);
+        return locationRequest;
+    }
+
+    private void getCurrentLocation(){
+        checkPermission();
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        List<String> list = lm.getAllProviders();
+
+        for(int i = 0; i<list.size(); i++){
+            list.get(i);
+            lm.isProviderEnabled(list.get(i));
+        }
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        criteria.setAltitudeRequired(false);
+        criteria.setSpeedRequired(true);
+        criteria.setCostAllowed(true);
+        criteria.setBearingRequired(false);
+
+        String bestProvide = locationManager.getBestProvider(criteria, true);
+
+        Location location = null;
+
+        //매니저를 이용하여 위치정보 얻어오기
+        if (locationManager.isProviderEnabled("gps")) {
+            location = locationManager.getLastKnownLocation("gps");
+        }else if (locationManager.isProviderEnabled("network")){
+            location = locationManager.getLastKnownLocation("network");
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 3000L, (float) 10, (LocationListener) this);
+        }
+
+        if(location==null){
+            et.setText("못찾음");
+        }else{
+            //위도, 경도 얻어오기
+            double latitude=location.getLatitude();
+            double longitude=location.getLongitude();
+
+            System.out.println(latitude + " " + longitude);
+            getKakaoMap(latitude, longitude);
+            et.setText(latitude+" , "+longitude);
+        }
+    }
+
+    private void getKakaoMap(double latitude, double longitude){
 
         MapView mapView = findViewById(R.id.map_view);
         mapView.start(new MapLifeCycleCallback() {
@@ -75,12 +144,26 @@ public class nearLawCenter extends AppCompatActivity {
                 // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출됨
             }
         }, new KakaoMapReadyCallback() {
+
             @Override
             public void onMapReady(KakaoMap kakaoMap) {
                 // 인증 후 API 가 정상적으로 실행될 때 호출됨
+
+            }
+            @Override
+            public LatLng getPosition() {
+                // 지도 시작 시 위치 좌표를 설정
+                return LatLng.from(latitude, longitude);
+            }
+
+            @Override
+            public int getZoomLevel() {
+                // 지도 시작 시 확대/축소 줌 레벨 설정
+                return 15;
             }
         });
     }
+
     private void checkPermission() {
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED &&
